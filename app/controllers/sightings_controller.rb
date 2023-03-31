661f5275b85118076a30218eb67fbea6ca18596d
Melaza6 <cueroleo6@gmail.com>
2023-03-31 10:57:38 -0700

    can see animals with asso sightings and a given time

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,6 @@
 class SightingsController < ApplicationController
     def index
-        sights = Sighting.all 
+        sights = Sighting.where(date: params[:start_date]..params[:end_date])
         render json: sights
     end
 
@@ -39,6 +39,9 @@ class SightingsController < ApplicationController
 
 private
     def sight_params
-        params.require(:sighting).permit(:latitude, :longitude, :date)
+        params.require(:sighting).permit(:animal_id, :latitude, :longitude, :date)
+    end
+    def sight_params_range
+        params.require(:sighting).permit(animal_id, :latitude, :longitude, :date, start_date, end_date)
     end
 end
